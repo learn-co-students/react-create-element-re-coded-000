55e23056daf870f4223cce3a446213342d839dc5
Ali Alrawi <ali.developer90@gmail.com>
2017-02-20 08:07:59 +0300

    Done.

---
+++ test/index-test.js
@@ -0,0 +1,20 @@
+const expect = require('expect')
+
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+
+
+describe('index', () => {
+  
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+  })
+  
+
+  it('runs', () => {
+
+    setTimeout(function(){}, 15000);
+    expect(true).toEqual(true)
+  })
+})
